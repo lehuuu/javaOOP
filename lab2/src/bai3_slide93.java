6f3eb56148ec70fdd7bf25d649f414dea203ba2f
lehuuu <caoy4399@gmail.com>
2024-01-24 12:47:20 +0700

    baitaplab2

---
+++ lab2/src/bai3_slide93.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class bai3_slide93 {
+    public static void main(String[] args) {
+      int tuoi;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("nhap ten :");
+        String ten = sc.nextLine();
+        System.out.println("nhap tuoi :");
+        tuoi = sc.nextInt();
+        if(tuoi < 16){
+            System.out.println("ban " + ten + " o tuoi vi thanh nien");
+        } else if(tuoi >=16) {
+            System.out.println("ban " + ten + " o tuoi truong thanh");
+        } else if(tuoi >= 18) {
+            System.out.println("ban " + ten + " da gia");
+        }
+    }
+}
